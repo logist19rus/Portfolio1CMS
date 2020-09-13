d73b9e7f3f9d3c4f72df66dee0c9bcb875818f00
Руслан Шахмаев <cfpro99@list.ru>
2020-09-13 12:32:01 +0700

    Подготовительная загрузка

---
+++ Portfolio1CMS/Controllers/Authorize.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio1CMS.Controllers
+{
+    public class Authorize : Controller
+    {
+        // GET: Authorize
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Auth()
+        {
+            return View();
+        }
+
+        // GET: Authorize/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Authorize/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authorize/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Authorize/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Authorize/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Authorize/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Authorize/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Portfolio1CMS/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portfolio1CMS.Models;
+
+namespace Portfolio1CMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GetIntro()
+        {
+            return PartialView("_GetIntro");
+        }
+        public IActionResult GetAdvantages()
+        {
+            return PartialView("_GetAdvantages");
+        }
+        public IActionResult GetInfo()
+        {
+            return PartialView("_GetInfo");
+        }
+        public IActionResult GetServices()
+        {
+            return PartialView("_GetServices");
+        }
+        public IActionResult GetFeedback()
+        {
+            return PartialView("_GetFeedback");
+        }
+        public IActionResult GetTeam()
+        {
+            return PartialView("_GetTeam");
+        }
+        public IActionResult GetContact()
+        {
+            return PartialView("_GetContact");
+        }
+    }
+}
